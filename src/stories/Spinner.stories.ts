f3be73c537d6d38b080a2aeec511eca3003ff885
David Requeno <davidrequeno52@gmail.com>
2025-02-05 23:44:34 -0600

    feat: add Spinner component with customizable size and color

---
+++ src/stories/Spinner.stories.ts
@@ -0,0 +1,40 @@
+import { Spinner } from "@/components/General/Spinner/Spinner";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof Spinner> = {
+  title: "General/Spinner",
+  component: Spinner,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    size: { control: "select", options: ["sm", "md", "lg"] },
+    color: { control: "text" },
+    className: { control: false },
+  },
+} satisfies Meta<typeof Spinner>;
+
+export default meta;
+type Story = StoryObj<typeof Spinner>;
+
+export const Default: Story = {
+  args: {
+    size: "md",
+    color: "border-t-green-400",
+  },
+};
+
+export const Small: Story = {
+  args: {
+    size: "sm",
+    color: "border-t-blue-400",
+  },
+};
+
+export const Large: Story = {
+  args: {
+    size: "lg",
+    color: "border-t-red-400",
+  },
+};
