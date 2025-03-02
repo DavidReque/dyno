a59c6f226922d198fb8371e557a9549828a2e51c
David Requeno <davidrequeno52@gmail.com>
2025-03-01 21:56:04 -0600

    feat: enhance Typography component with spacing adjustments and color mapping updates

--- src/components/General/Typography/Typography.tsx
+++ src/components/General/Typography/Typography.tsx
@@ -1,10 +1,9 @@
-import { cn } from "@/lib/utils";
 import React from "react";
+import { cn } from "@/lib/utils";
+import { Spacing } from "@/theme/spacing";
 
 export interface TypographyProps {
-  /** HTML element to render */
   as?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "span";
-  /** Variant defines the text style regardless of the HTML element */
   variant?:
     | "display"
     | "h1"
@@ -15,13 +14,9 @@ export interface TypographyProps {
     | "caption"
     | "code"
     | "blockquote";
-  /** Custom weight of the text */
   weight?: "regular" | "medium" | "semibold" | "bold" | "extrabold";
-  /** Color variant */
   color?: "default" | "primary" | "secondary" | "success" | "error" | "warning";
-  /** Optional additional className */
   className?: string;
-  /** Content */
   children: React.ReactNode;
 }
 
@@ -34,18 +29,24 @@ export const Typography = ({
   children,
   ...props
 }: TypographyProps) => {
-  const variantStyles = {
+  const variantClasses = {
     display: "text-5xl leading-tight",
     h1: "scroll-m-20 text-4xl tracking-tight lg:text-5xl",
     h2: "scroll-m-20 border-b pb-2 text-3xl tracking-tight first:mt-0",
     h3: "scroll-m-20 text-2xl tracking-tight",
     h4: "scroll-m-20 text-xl tracking-tight",
-    body1: "leading-7 [&:not(:first-child)]:mt-6",
-    blockquote: "mt-6 border-l-2 pl-6 italic",
+    body1: "leading-7",
+    blockquote: "border-l-2 pl-6 italic",
     caption: "text-xs leading-normal",
     code: "relative rounded bg-gray-500 px-[0.3rem] py-[0.2rem] font-mono text-sm font-semibold",
   };
 
+  const spacingInlineStyle: React.CSSProperties = {};
+  if (variant === "body1") {
+    spacingInlineStyle.marginTop = Spacing.md; // ~16px
+  }
+
+  // Definimos el mapeo de pesos
   const weightStyles = {
     regular: "font-normal",
     medium: "font-medium",
@@ -54,23 +55,25 @@ export const Typography = ({
     extrabold: "font-extrabold",
   };
 
+  // Mapeo de colores
   const colorStyles = {
     default: "text-neutral-900",
     primary: "text-white",
     secondary: "text-neutral-600",
     success: "text-green-600",
-    error: "text-destructive-600",
+    error: "text-red-600",
     warning: "text-yellow-600",
   };
 
   return (
     <Component
       className={cn(
-        variantStyles[variant],
+        variantClasses[variant],
         weightStyles[weight],
         colorStyles[color],
         className
       )}
+      style={spacingInlineStyle}
       {...props}
     >
       {children}
